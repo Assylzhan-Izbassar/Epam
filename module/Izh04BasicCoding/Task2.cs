32631b17d43d0fd3c11a3986fe2094af46de9236
Assylzhan-Izbassar <super_boy.13@mail.ru>
2019-10-14 15:39:56 +0600

    module1

---
+++ module/Izh04BasicCoding/Task2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Izh04BasicCoding
+{
+    class Task2
+    {
+        int maxIndex = 0; int index = 0;
+        public int CheckMaxElementFinding(int[] sourceArray)
+        {
+            if (sourceArray == null)
+                return 0;
+            else if (index == sourceArray.Length)
+            {
+                return sourceArray[maxIndex];
+            }
+            else if (sourceArray[index] > sourceArray[maxIndex])
+            {
+                maxIndex = index;
+            }
+            index++;
+
+            return CheckMaxElementFinding(sourceArray);
+        }
+    }
+}
