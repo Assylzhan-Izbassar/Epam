c79029072b8ac73f9e688eb9eec2311ad33d7fba
Assylzhan-Izbassar <super_boy.13@mail.ru>
2019-10-14 21:38:33 +0600

    Task5

--- module/Izh04BasicCoding/Izh04BasicCodingTest.cs
+++ module/Izh04BasicCoding/Izh04BasicCodingTest.cs
@@ -53,5 +53,23 @@ namespace Izh04BasicCoding
             //Assert
             return task.FilterDigit(sourceArray, digit);//Add
         }
+        [TestCase(12, ExpectedResult = 21)]
+        [TestCase(513, ExpectedResult = 531)]
+        [TestCase(2017, ExpectedResult = 2071)]
+        [TestCase(414, ExpectedResult = 441)]
+        [TestCase(144, ExpectedResult = 414)]
+        [TestCase(1234321, ExpectedResult = 1241233)]
+        [TestCase(1234126, ExpectedResult = 1234162)]
+        [TestCase(3456432, ExpectedResult = 3462345)]
+        [TestCase(10, ExpectedResult = -1)]           	
+        [TestCase(20, ExpectedResult = -1)]
+
+        public int CheckFindNextBiggerNumber(int number)
+        {
+            //Arrange
+            Task5 task = new Task5();
+            //Assert
+            return task.FindNextBiggerNumber(number);//Add
+        }
     }
 }
--- module/Izh04BasicCoding/Task2.cs
+++ module/Izh04BasicCoding/Task2.cs
@@ -11,19 +11,19 @@ namespace Izh04BasicCoding
         int maxIndex = 0; int index = 0;
         public int MaxElementFinding(int[] sourceArray)
         {
-            if (sourceArray == null)
-                return 0;
-            else if (index == sourceArray.Length)
+            if (sourceArray == null)//if our array is empty, then we return -1;
+                return -1;
+            else if (index == sourceArray.Length)//if our index come to the array size, then we return element
             {
                 return sourceArray[maxIndex];
             }
-            else if (sourceArray[index] > sourceArray[maxIndex])
+            else if (sourceArray[index] > sourceArray[maxIndex])//if we have more element than we had then our maxi index will be new index
             {
                 maxIndex = index;
             }
             index++;
 
-            return MaxElementFinding(sourceArray);
+            return MaxElementFinding(sourceArray);//recursive call
         }
     }
 }
