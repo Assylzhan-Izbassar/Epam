278a3c651400b78b4dce1fdc167b49f4d789eab4
Assylzhan-Izbassar <super_boy.13@mail.ru>
2019-11-10 22:47:10 +0600

    IzhForAsync

---
+++ module5/Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Sum sum = new Sum();
+
+            sum.CalcSum(n);
+
+            while(true)
+            {
+                if(sum.GetResult() == true)
+                {
+                    Console.WriteLine("Sum is complited:");
+                    break;
+                }
+                else
+                { 
+                    try
+                    {
+                        n = int.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                    }
+                    sum.CalcSum(n);
+                }
+            }
+            Console.WriteLine(sum.GetResultSum());
+        }
+    }
+}
---
+++ module5/Task1/Sum.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Sum
+    {
+        private bool result { get; set; }
+        private int resultSum = 0;
+        public Sum()
+        {
+            result = false;
+        }
+
+        /// <summary>
+        /// This asunc method is calculate a sum of the n numbers.
+        /// </summary>
+        /// <param name="n"></param>
+        public async void CalcSum(int n)
+        {
+            int sum = 0;
+
+            if (n >= 0)
+            {
+                for (int i = 0; i <= n; ++i)
+                {
+                    Console.SetCursorPosition(0, 0);
+                    sum += i;
+                    await Task.Delay(100);
+                    Console.Write(sum.ToString());
+                }
+                resultSum = sum;
+                result = true;
+            }
+            else
+            {
+                for (int i = n; i <= 0; ++i)
+                {
+                    sum += i;
+                    await Task.Delay(100);
+                    Console.Write(sum.ToString());
+                }
+                resultSum = sum;
+            }
+            Console.WriteLine();
+        }
+        public bool GetResult()
+        {
+            return this.result;
+        }
+        public int GetResultSum()
+        {
+            return this.resultSum;
+        }
+    }
+}
