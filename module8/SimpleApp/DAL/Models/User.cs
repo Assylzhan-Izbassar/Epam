18534280ae296de18c9ab03575bd4ca5b9001438
Assylzhan-Izbassar <super_boy.13@mail.ru>
2020-01-03 13:00:17 +0600

    SimpleMVCApp

---
+++ module8/SimpleApp/DAL/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ module8/SimpleApp/DAL/Repository/IAwardRepository.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repository
+{
+    interface IAwardRepository:IDisposable
+    {
+        IEnumerable<User> GetUsers();
+        User GetUserByID(int userID);
+        bool AddUser(User user);
+        bool UpdateUser(User user);
+        bool DeleteUser(int userID);
+        void Save();
+
+        IEnumerable<Award> GetAwards();
+        Award GetAwardByID(int awardID);
+        bool AddAward(Award award);
+        bool UpdateAward(Award award);
+        bool DeleteAward(int awardID);
+    }
+}
