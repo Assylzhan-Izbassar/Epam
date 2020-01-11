619ad6ffa9ccbd136b7bcb1b8016cf9c76f0a5dd
Assylzhan-Izbassar <super_boy.13@mail.ru>
2020-01-12 00:17:31 +0600

    ChangedTask6

---
+++ module10/Task6/Task6/Controllers/AccountController.cs
@@ -0,0 +1,133 @@
+﻿using System.Data;
+using DAL.Staff;
+using DAL.Models;
+using DAL.Repository;
+using Task6.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task6.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IAwardRepository repository;
+
+        public AccountController()
+        {
+            repository = new AwardRepository(new AwardDbContext());
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    User user = await repository.GetUsers().FirstOrDefaultAsync(u => u.Email == model.Email);
+
+                    if (user == null)
+                    {
+                        user = new User 
+                        { 
+                            Name = model.Name,
+                            Birthdate = model.Birthdate,
+                            Email = model.Email, 
+                            Password = model.Password 
+                        };
+
+                        Role userRole = await repository.GetRoles().FirstOrDefaultAsync(r => r.Name == "user");
+
+                        if (userRole != null)
+                            user.Role = userRole;
+
+                        repository.AddUser(user);
+                        repository.SaveAsync();
+
+                        await Authenticate(user);
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                        ModelState.AddModelError("", "Incorrect login or password!");
+                }
+            }
+            catch(DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginModel { ReturnUrl = returnUrl});
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    User user = await repository.GetUsers()
+                        .Include(u => u.Role)
+                        .FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+
+                    if (user != null)
+                    {
+                        await Authenticate(user);
+
+                        return Redirect(model.ReturnUrl ?? Url.Action("Index", "Home"));
+                    }
+                    ModelState.AddModelError("", "Incorrect login or password!");
+                }
+            }
+            catch(DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
+
+        private async Task Authenticate(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role?.Name)
+            };
+
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+    }
+}
---
+++ module10/Task6/Task6/Controllers/AwardController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using DAL.Staff;
+using DAL.Models;
+using System.Data;
+using DAL.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using DAL.Repository;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Task6.Controllers
+{
+    [Authorize]
+    public class AwardController : Controller
+    {
+        private IAwardRepository Repository;
+        [Obsolete]
+        private IHostingEnvironment hosting;
+
+        [Obsolete]
+        public AwardController(IHostingEnvironment hosting)
+        {
+            Repository = new AwardRepository(new AwardDbContext());
+            this.hosting = hosting;
+        }
+
+        public IActionResult Index()
+        {
+            var awards = from award in Repository.GetAwards()
+                         select award;
+            return View(awards);
+        }
+
+        public ViewResult Details(int id)
+        {
+            return View(Repository.GetAwardByID(id));
+        }
+        [Authorize(Roles = "admin")]
+        public ActionResult Create()
+        {
+            return View(new AwardView());
+        }
+        [HttpPost]
+        [Obsolete]
+        [Authorize(Roles = "admin")]
+        public ActionResult Create(AwardView awardView)
+        {
+            Award award = null;
+
+            try
+            {
+                if(ModelState.IsValid && awardView.Image != null)
+                {
+                    string fileName = SetPhotoPath(awardView);
+                    string path = "/img/" + Path.GetFileName(awardView.Image.FileName);
+
+                    award = new Award()
+                    {
+                        Title = awardView.Title,
+                        Description = awardView.Description,
+                        ImagePath = path
+                    };
+
+                    Repository.AddAward(award);
+                    Repository.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(award);
+        }
+        [Authorize(Roles = "admin")]
+        public ActionResult Delete(int id, bool? saveChangesError)
+        {
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Unable to save changes. Try again, and if the problem persists see your system administrator.";
+            }
+            Award award = Repository.GetAwardByID(id);
+            return View(award);
+        }
+        [HttpPost, ActionName("Delete")]
+        [Authorize(Roles = "admin")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                Repository.DeleteAward(id);
+                Repository.Save();
+            }
+            catch(DataException)
+            {
+                return RedirectToAction("Delete", new { id, V = true });
+            }
+            return RedirectToAction("Index");
+        }
+
+        [Obsolete]
+        private string SetPhotoPath(AwardView awardView)
+        {
+            string fileName = Path
+                        .Combine(hosting.WebRootPath, "img", Path.GetFileName(awardView.Image.FileName));
+            using (var stream = new FileStream(fileName, FileMode.Create))
+            {
+                awardView.Image.CopyTo(stream);
+            }
+
+            return fileName;
+        }
+
+        public ViewResult Edit(int id)
+        {
+            Award award = Repository.GetAwardByID(id);
+
+            AwardEditView view = new AwardEditView
+            {
+                Id = award.AwardID,
+                Title = award.Title,
+                Description = award.Description,
+                ExistingPath = award.ImagePath
+            };
+
+            return View(view);
+        }
+
+        [HttpPost]
+        [Obsolete]
+        [Authorize(Roles = "admin")]
+        public ActionResult Edit(AwardEditView awardView)
+        {
+            Award award = null;
+
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    award = Repository.GetAwardByID(awardView.Id);
+
+                    award.Title = awardView.Title;
+                    award.Description = awardView.Description;
+
+                    if (awardView.Image != null)
+                    {
+                        if(awardView.ExistingPath != null)
+                        {
+                            string oldPath = Path
+                                .Combine(@"C:\Users\Brother\Desktop\Epam\module8\SimpleApp\SimpleApp\wwwroot\img", Path.GetFileName(awardView.Image.FileName));
+                            System.IO.File.Delete(oldPath);
+                        }
+                        string filePath = SetPhotoPath(awardView);
+                        string path = "/img/" + Path.GetFileName(awardView.Image.FileName);
+                        award.ImagePath = path;
+                    }
+
+                    Repository.UpdateAward(award);
+                    Repository.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(award);
+        }
+
+    }
+}
